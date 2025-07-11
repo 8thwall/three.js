8bd8a076ff55fcce20333a9c7421704afeb0313e
Owen Mech <owen@nianticspatial.com>
2025-07-11 11:19:37 -0700

    consistent style

--- src/renderers/webgl/WebGLRenderLists.js
+++ src/renderers/webgl/WebGLRenderLists.js
@@ -186,7 +186,7 @@ function WebGLRenderList() {
 		transmissive: transmissive,
 		transparent: transparent,
 
-		objectIdToRenderItem,
+		objectIdToRenderItem: objectIdToRenderItem,
 
 		init: init,
 		push: push,
